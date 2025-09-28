ba99967c8820af38652bf3dcd3d7a3fd126c751f
your name <you@example.com>
2025-09-28 15:39:28 -0400

    Création table tickets avec Laravel

---
+++ database/migrations/2025_09_28_193156_create_tickets_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->string('vol_id');
+            $table->foreign('vol_id')
+              ->references('id')
+              ->on('vols')
+              ->onUpdate('cascade')
+              ->onDelete('cascade');
+
+              $table->foreignId('user_id')
+              ->references('id')
+              ->on('users')
+              ->onUpdate('cascade')
+              ->onDelete('cascade');
+        });
+        
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+}
