30f4aed05309a10518910fddbfc37243a7ebb86d
your name <you@example.com>
2025-09-28 15:31:03 -0400

    Création table vols avec Laravel

---
+++ database/migrations/2025_09_28_191915_create_vols_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVolsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vols', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->dateTime('date_depart');
+            $table->dateTime('date_arrive');
+            $table->string('origine', 255);
+            $table->string('destination', 255);
+            $table->integer('prix');
+            $table->boolean('efface')->default(0);
+            $table->timestamps();
+
+            $table->foreignId('avion_id')
+            ->references('id')
+            ->on('avions')
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vols');
+    }
+}
