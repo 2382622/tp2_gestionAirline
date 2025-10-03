e563c288ca545fa712e1993d79dbd53984ac353e
your name <you@example.com>
2025-10-03 09:52:46 -0400

    Création d'utilisateurs via les seeders et modification du fichier DatabaseSeeder pour lancer la création de user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,16 +3,20 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
-        //
+        // Un admin fixe
+        User::factory()->create([
+            'name'   => 'Admin',
+            'prenom' => 'Test',
+            'email'  => 'admin@example.com',
+            'password' => bcrypt('123456'),
+            'role'   => 'admin',
+        ]);
+
     }
 }
