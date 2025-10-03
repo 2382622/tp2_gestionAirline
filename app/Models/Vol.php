50974a69d95b3c3f2f8d01357e5c9e460a2e8403
your name <you@example.com>
2025-10-03 11:49:12 -0400

    Implentation du modele pour les vols et de la relation

--- app/Models/Vol.php
+++ app/Models/Vol.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Vol extends Model
 {
     use HasFactory;
+
+    // Comme la clé primaire est un string (id du vol) donc pas de autoIncrememnt
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'id',
+        'date_depart',
+        'date_arrive',
+        'origine',
+        'destination',
+        'prix',
+        'efface',
+        'avion_id',
+    ];
+
+    // Relation : un vol appartient à un avion
+    public function avion()
+    {
+        return $this->belongsTo(Avion::class);
+    }
 }
